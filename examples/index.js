c8ed90f3968709832a73192f7a6c3bf7ed7bf1fb
mawencan <1819293933@qq.com>
2022-03-20 18:19:49 +0800

    test: 调整测试

--- examples/index.js
+++ examples/index.js
@@ -1,3 +1,12 @@
 import { getPackagesDir } from '../index.js'
 getPackagesDir()
-  .then(res => console.log(res))
+  .then(({ dirs, filesPath}) => {
+    const _dirs = [ 'packages/a', 'packages/b', 'packages/cc' ]
+    const _filesPath = [
+      'packages/a/package.json',
+      'packages/b/package.json',
+      'packages/cc/package.json'
+    ]
+    console.log(dirs.every((item, index) => item === _dirs[index]))
+    console.log(filesPath.every((item, index) => item === _filesPath[index]))
+  })
