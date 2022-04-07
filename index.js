26df70a8bcf0a2af64536f0dff3578142610dfbd
mawencan <1819293933@qq.com>
2022-04-07 10:48:17 +0800

    fix: 修复node_modules问题

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import { globby } from 'globby'
 export async function getPackagesDir(packagesPath) {
-  const values = await globby(packagesPath)
+  const ignore = '!**/node_modules'
+  const params = Array.isArray(packagesPath) ? [...packagesPath, ignore] : [packagesPath, ignore]
+  const values = await globby(params)
   const regExp = new RegExp('\/package\\.json$')
   const filesPath = values.filter(item => regExp.test(item))
   const dirs = filesPath.map(item => item.replace(regExp, ''))
